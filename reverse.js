ea75ffda417aee57200b683e19ae8906ec81591a
Mohammad <mh.mozaffari1985@gmail.com>
2021-06-21 22:25:07 +0000

    reverse.js deleted

--- reverse.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-for (let i = 2; i < process.argv.length; i++) {
-  console.log(reverseString(process.argv[i]));
-}
-
-const reverseString = function(str) {
-  return str.split("").reverse().join("");
-};
